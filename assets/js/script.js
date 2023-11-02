810e26cb673d5fa32ab003382c6ece3ce6d6ca31
Alex Hoenig <delimey@pm.me>
2023-11-02 11:39:34 +0000

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,11 +32,20 @@ function runGame(gameType) {
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
+        
         displayAdditionQuestion(num1, num2);
+
+    } else if (gameType === "multiply") {
+
+        displayMultiplyQuestion(num1, num2);
+
     } else {
+
         alert (`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
+
 }
 
 /**
@@ -70,6 +79,8 @@ function calculateCorrectAnswer() {
 
         if (operator === "+") {
             return [operand1 + operand2, "addition"];
+        } else if (operator === "x") {
+            return [operand1 * operand2, "multiply"];
         } else {
             alert(`operator not known ${operator}`);
             throw `operator not known ${operator}. Aborting!`;
@@ -103,8 +114,12 @@ function displaySubtractQuestion() {
     
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
     
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
 }
 
 function displayDivideQuestion() {
