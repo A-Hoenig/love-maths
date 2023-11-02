5eb09631b49986b6dd6118553ea127ccdbf1cf66
Alex Hoenig <delimey@pm.me>
2023-11-02 11:49:32 +0000

    add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,10 @@ function runGame(gameType) {
 
         displayMultiplyQuestion(num1, num2);
 
+    } else if (gameType === "subtract") {
+
+        displaySubtractQuestion(num1, num2);
+
     } else {
 
         alert (`Unknown game type: ${gameType}`);
@@ -81,6 +85,8 @@ function calculateCorrectAnswer() {
             return [operand1 + operand2, "addition"];
         } else if (operator === "x") {
             return [operand1 * operand2, "multiply"];
+        } else if (operator === "-") {
+            return [operand1 - operand2, "subtract"];
         } else {
             alert(`operator not known ${operator}`);
             throw `operator not known ${operator}. Aborting!`;
@@ -110,8 +116,13 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
     
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+
+
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
